843cf71b694e0b6fd8df84b86bc36c68f70afd94
Brian Smawley <briansmawley@gmail.com>
2022-11-29 15:45:51 -0500

    Making progress on tests
    still a lot of errors.

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -2,51 +2,33 @@ import React from 'react';
 import Event from '../Event';
 import { shallow } from 'enzyme';
 import { mockData } from '../mock-data';
-import { fireEvent } from '@testing-library/react';
-import { render } from '@testing-library/react';
 
 
 describe('<Event /> component', () => {
     let EventWrapper;
+    const event = mockData[0];
     beforeAll(() => {
-    EventWrapper = render(<Event />);
+    EventWrapper = shallow(<Event event={event} />);
     });    
 
 
+    test('Eventwrapper is defined /> component', () => {
+        expect(EventWrapper).toBeDefined();
+    });
+
     test('<Ensure Div is rendered /> component', () => {
-        expect(EventWrapper.find('.event').length).toEqual(1);
+        expect(EventWrapper.find('.event')).toHaveLength(1);
     })
 
     test('<Event Title is rendered /> component', ()=> {
-        expect(EventWrapper.find('#title')).toHaveLength(1);
-    });
+        expect(EventWrapper.find('.summary')).toBe(1);
+    })
+
+    test('renders the summary as a header /> component', () =>{
+        expect(EventWrapper.find('.summary')).toBe(h1.summary);
+    })
+
+
 });
 
 
-
-    // test('<Ensure event title is rendered properly /> component'() => {
-    //     expect
-    // })
-
-//     test('Button opens/closes dropdown on click', () => {
-//         const {button} = shallow(render(<mockData />));
-//         expect(showDetails).tobe(true);
-//         fireEvent.click(button);
-//     });
-
-    // test('<loads details from mockData /> component', () => {
-    //     test('the details are loaded from mockData')
-    // }
-
-    // test('<dropdown shows correct details />, component' () => {
-    //     test('the details of the loaded mockData matches the event')
-    //     const button = 
-    //     expect(button.length).toBe(1);
-    //     button.simulate('click');
-    // })
-
-    // test("renders collapsed by default", () => {
-        
-
-  
-// });
--- src/event.js
+++ src/event.js
@@ -1,12 +1,48 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  state = { collapsed: true };
+  toggleDetails = () => {
+    this.setState((prevState) => ({
+      collapsed: !prevState.collapsed,
+    }));
+  };
+  
+  
+  
   render() {
-    return <div class='event'>
-      <div id="title">
-        Title
-      </div>
-      <button class='details-button'>
+    const { event } = this.props;
+    const { collapsed } = this.state;
+    return <div>
+      <h1 class="summary">
+        {event.summary}
+      </h1>
+    
+      <p className="event-address">
+        {`@${event.summary} | ${event.location}`}
+    {/* 
+    type literal that is combining the summary & location into a single paragraph block 
+    */ }
+      </p>{
+        !collapsed &&(
+          <>
+          <h2 className="about"> About Event:</h2>
+          <a className="link" href={event.htmlLink}>
+            Click Here to See Additional Details!
+            </a>
+            <p className="description">
+              {event.description}
+            </p>
+            </>
+        )
+      }
+      <p classname="event-start-time">
+        {new Date(event.start.dateTime).toString()}
+      </p>
+    
+      <button class='details-button' onClick={() =>
+         this.toggleDetails()}>
+          {collapsed ? "show" : "hide"} 
             See Details
       </button>
     </div>;
