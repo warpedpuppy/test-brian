d5318cdd37372dc09b3e63f59478be0c10e1acf6
Brian Smawley <briansmawley@gmail.com>
2022-11-28 20:21:26 -0500

    broken tests

--- src/event.js
+++ src/event.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 
 class Event extends Component {
   render() {
-    return <div></div>;
+    return <div class='event'>
+      <div id="title">
+        Title
+      </div>
+      <button class='details-button'>
+            See Details
+      </button>
+    </div>;
   }
 }
 export default Event;
