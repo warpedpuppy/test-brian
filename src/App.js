8da31b7e5f25d11b81ece3c6646f279c1dc41fe0
Brian Smawley <briansmawley@gmail.com>
2022-11-30 15:11:18 -0500

    Integration testing
    1 error with importing issues

--- src/App.js
+++ src/App.js
@@ -3,15 +3,30 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from "./NumberOfEvents";
-import { mockData } from "./mock-data";
+import getEvents from './api';
 
 class App extends Component {
+  state = {
+    events: [],
+    locations:[]
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents />
-        <EventList events={mockData} />
+        <EventList events={this.state.events} />
       </div>
     );
   }
