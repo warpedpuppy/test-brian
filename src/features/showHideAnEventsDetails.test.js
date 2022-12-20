1ce1d1ff33bd351c6efaaa51b7f1c208962d1576
Brian Smawley <briansmawley@gmail.com>
2022-12-20 12:03:14 -0500

    Finalized fixed tests

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -7,12 +7,12 @@ import  Event  from '../Event';
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
 
 defineFeature(feature, test => {
+    let AppWrapper;
     test('An event element is collapsed by default', ({ given, when, then }) => {
         given('The user open the app', () => {
     
         });
     
-        let AppWrapper;
         when('The user lands on the main page', () => {
           AppWrapper = mount(<App />);
         });
@@ -24,35 +24,35 @@ defineFeature(feature, test => {
     });
 
     test('User can expand an event to see its details', ({ given, when, then }) => {
-        let AppWrapper;
         given('a user is viewing the main page', () => {
             AppWrapper = mount(<App />);
         });
             
-        when('the user clicks the button to see more details', async () => {
-            await AppWrapper.find("event", " .details-button").at(0).simulate('click');
+        when('the user clicks the button to see more details',  () => {
+            AppWrapper.update();
+            AppWrapper.find(".event .details-button").at(0).simulate('click');
         });
     
-        then('the event is expanded to show the details', async () => {
-            let EventWrapper = AppWrapper.find(".event")
-            expect(EventWrapper.find(" .description").at(0)).toHaveLength(1);
+        then('the event is expanded to show the details',  () => {
+            expect(AppWrapper.find('.event .description')).toHaveLength(1);
+
         });
     });
 
     test('User can collapse an event to hide its details', ({ given, when, then }) => {
-        let AppWrapper;
-        given('the user is viewing an expanded event', () => {
-            AppWrapper = mount(<App />);
-            AppWrapper.find("event", " .details-button").at(0).simulate('click');
+        given('the user is viewing an expanded event', async () => {
+            AppWrapper = await mount(<App />);
+            AppWrapper.update();
+            AppWrapper.find(".event .details-button").at(0).simulate('click');
         });
     
         when('the user clicks the close details button', () => {
-            AppWrapper.find(".event", " .details-button").at(0).simulate('click');
+            AppWrapper.update();
+            AppWrapper.find(".event .details-button").at(0).simulate('click');
         });
     
         then('the event collapses back to it\'s initial state', () => {
-            let EventWrapper = AppWrapper.find(".event")
-            expect(EventWrapper.find(".description").at(0)).toHaveLength(0);
+            expect(AppWrapper.find(".event .description")).toHaveLength(0);
         });
     });
 });
