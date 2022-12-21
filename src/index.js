63e0cca3bc208636f4ac8d00939f6deed9581a48
Brian Smawley <briansmawley@gmail.com>
2022-12-21 14:39:55 -0500

    atatus integration

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
+import * as atatus from 'atatus-spa';
 
+atatus.config('0f6f03e693674951abadc54720220097').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
