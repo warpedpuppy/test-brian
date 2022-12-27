bcce69fb6753b5671060fb69ba622212aefffde4
Brian Smawley <briansmawley@gmail.com>
2022-12-27 18:22:27 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -43,7 +43,7 @@ const checkToken = async (accessToken) => {
     if (!navigator.onLine) {
       const data = localStorage.getItem("lastEvents");
       NProgress.done();
-      return data ? JSON.parse(events).events : [];
+      return data ? JSON.parse(data).events : [];
     }
 
     const token = await getAccessToken();
