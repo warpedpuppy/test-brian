402d2c1d73a98f928e6eb1a9144c0aec595870a2
Brian Smawley <briansmawley@gmail.com>
2022-12-27 19:25:52 -0500

    Syntax and casing fixes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
 import "./styles/nprogress.css";
 import { InfoAlert } from "./Alert";
-import WelcomeScreen from "./styles/WelcomeScreen.css";
+import WelcomeScreen from './WelcomeScreen';
 import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
 
 class App extends Component {
@@ -27,11 +27,34 @@ class App extends Component {
           ? events
           : events.filter((event) => event.location === location);
       this.setState({
+        showWelcomeScreen: undefined,
         events: locationEvents.slice(0, eventCount),
         //the slice begins at 0, the first entry, and runs to 1 less than eventCount
       });
     });
   };
+
+
+  async componentDidMount() {
+    this.mounted = true;
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
 
   render() {
     if (this.state.showWelcomeScreen === undefined)
@@ -58,25 +81,6 @@ class App extends Component {
         />
       </div>
     );
-  }
-  async componentDidMount() {
-    this.mounted = true;
-    const accessToken = localStorage.getItem("access_token");
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
-      getEvents().then((events) => {
-        if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
-        }
-      });
-    }
-  }
-
-  componentWillUnmount() {
-    this.mounted = false;
   }
 }
 
