9b9af7d0d5771a08f56e5f5eeb40b90f556f68ba
Brian Smawley <briansmawley@gmail.com>
2022-12-29 11:47:49 -0500

    adjusting state

--- src/App.js
+++ src/App.js
@@ -44,9 +44,13 @@ class App extends Component {
     if ((code || isTokenValid) && this.mounted) {
       getEvents().then((events) => {
         if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
+          this.setState({ 
+            events: events.slice(0, this.state.numberOfEvents),
+            locations: extractLocations(events) 
+          });
         }
       });
+      
     }
   }
 
