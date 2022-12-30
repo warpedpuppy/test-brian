884f118e94977b44bb55dd7c55213ce419dc53d1
Brian Smawley <briansmawley@gmail.com>
2022-12-29 22:02:10 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,107 +1,103 @@
-import { mockData } from "./mock-data";
-import axios from "axios";
-import NProgress from "nprogress";
+import { mockData } from './mock-data';
+import axios from 'axios';
+import NProgress from 'nprogress';
 
 export const getAccessToken = async () => {
-  const access_token = localStorage.getItem("access_token");
-  const tokenCheck = access_token && (await checkToken(access_token));
+    const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
 
-  if (!access_token || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const results = await axios.get(
-        "https://7zumlswfif.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
-      );
-      const { authUrl } = results.data;
-      return (window.location.href = authUrl);
-    }
-    return code && getToken(code);
-  }
-  return access_token;
-};
-
-export const checkToken = async (access_token) => {
-  const result = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${access_token}`
-  )
-    .then((res) => res.json())
-    .catch((error) => error.json());
-
-  return result;
-};
-
-export const getEvents = async () => {
-  NProgress.start();
-
-  if (window.location.href.startsWith("http://localhost")) {
-    NProgress.done();
-    return mockData;
-  }
-
-  if (!navigator.onLine) {
-    const data = localStorage.getItem("lastEvents");
-    NProgress.done();
-    return data ? JSON.parse(data).events : [];
-  }
-
-  const token = await getAccessToken();
-  if (token) {
-    removeQuery();
-    try {
-      const url = `https://7zumlswfif.execute-api.us-east-1.amazonaws.com/dev/api/get-events/${token}`;
-      const result = await axios.get(url);
-      if (result.data) {
-        var locations = extractLocations(result.data.events);
-        localStorage.setItem("lastEvents", JSON.stringify(result.data));
-        localStorage.setItem("locations", JSON.stringify(locations));
+    if (!accessToken || tokenCheck.error) {
+      await localStorage.removeItem("access_token");
+      const searchParams = new URLSearchParams(window.location.search);
+      const code = await searchParams.get("code");
+      if (!code) {
+        const results = await axios.get(
+          "https://7zumlswfif.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
+        );
+        const { authUrl } = results.data;
+        return (window.location.href = authUrl);
       }
+      return code && getToken(code);
+    }
+    return accessToken;
+}
+
+ export const checkToken = async (accessToken) => {
+    const result = await fetch(
+      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    )
+      .then((res) => res.json())
+      .catch((error) => error.json());
+  
+    return result;
+  };
+
+  export const getEvents = async () => {
+    NProgress.start();
+  
+    if (window.location.href.startsWith("http://localhost")) {
       NProgress.done();
-      return result.data.events;
-    } catch (e) {
-      console.error(e);
       return mockData;
     }
-  }
-};
-
-const removeQuery = () => {
-  if (window.history.pushState && window.location.pathname) {
-    var newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState("", "", newurl);
-  } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
-  }
-};
-
-const getToken = async (code) => {
-  try {
-    const encodeCode = encodeURIComponent(code);
-
-    const response = await fetch(
-      "https://7zumlswfif.execute-api.us-east-1.amazonaws.com/dev/api/token" +
-        "/" +
-        encodeCode
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+  
+    if (!navigator.onLine) {
+      const data = localStorage.getItem("lastEvents");
+      NProgress.done();
+      return data ? JSON.parse(data).events : [];
     }
-    const { access_token } = await response.json();
-    access_token && localStorage.setItem("access_token", access_token);
-    return access_token;
-  } catch (error) {
-    error.json();
-  }
-};
 
-export const extractLocations = (events) => {
-  var extractLocations = events.map((event) => event.location);
-  var locations = [...new Set(extractLocations)];
-  return locations;
+    const token = await getAccessToken();
+    if (token) {
+      removeQuery();
+      try { 
+        const url = `https://7zumlswfif.execute-api.us-east-1.amazonaws.com/dev/api/get-events/${token}`;
+        const result = await axios.get(url);
+        if (result.data) {
+          var locations = extractLocations(result.data.events);
+          localStorage.setItem("lastEvents", JSON.stringify(result.data));
+          localStorage.setItem("locations", JSON.stringify(locations));
+        }
+        NProgress.done();
+        return result.data.events;
+      } catch(e) {
+        console.error(e);
+        return mockData;
+      }
+    }
+  };
+
+  const removeQuery = () => {
+    if (window.history.pushState && window.location.pathname) {
+      var newurl =
+        window.location.protocol +
+        "//" +
+        window.location.host +
+        window.location.pathname;
+      window.history.pushState("", "", newurl);
+    } else {
+      newurl = window.location.protocol + "//" + window.location.host;
+      window.history.pushState("", "", newurl);
+    }
+  };
+
+  const getToken = async (code) => {
+    try {
+        const encodeCode = encodeURIComponent(code);
+
+        const response = await fetch( 'https://7zumlswfif.execute-api.us-east-1.amazonaws.com/dev/api/token' + '/' + encodeCode);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const { access_token } = await response.json();
+        access_token && localStorage.setItem("access_token", access_token);
+        return access_token;
+    } catch(error) {
+        error.json();
+    }
+}
+
+ export const extractLocations = (events) => {
+    var extractLocations = events.map((event) => event.location);
+    var locations = [...new Set(extractLocations)];
+    return locations;
 };
